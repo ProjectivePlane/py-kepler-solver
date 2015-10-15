dd0e1bcd4c4b72bc18d4627c554528cc793f146f
ProjectivePlane <derek.bell.ie@gmail.com>
2015-10-15 23:43:26 +0100

    Pushing more tests.

--- test_solve.py
+++ test_solve.py
@@ -8,6 +8,7 @@ class TestKeplerSolver(unittest.TestCase):
     def test_eccentric_anomaly_solver(self):
         self.assertEqual(0.0,solve.solve_for_eccentric_anomaly(0,0))
         self.assertEqual(1.0885977582695519,solve.solve_for_eccentric_anomaly(1,0.1))
+        self.assertEqual(1.8620866869674508,solve.solve_for_eccentric_anomaly(1,0.9))
 
 if __name__=='__main__':
     unittest.main()
